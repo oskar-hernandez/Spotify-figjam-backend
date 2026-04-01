5f59c0701f5f993989efb597e3d137accc85ca46
oskar-hernandez <oskar.hernandez@factorial.co>
2026-04-01 23:31:41 +0200

    Change module export to ES6 syntax

--- api/get-token.js
+++ api/get-token.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   if (req.method === "OPTIONS") { res.status(200).end(); return; }
 
@@ -11,4 +11,4 @@ module.exports = async function handler(req, res) {
 
   const token = JSON.parse(redisData.result);
   res.status(200).json(token);
-};
+}
