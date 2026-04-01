433ae756d7d8998e67dcfa5277e0d0fb9f98c9da
oskar-hernandez <oskar.hernandez@factorial.co>
2026-04-02 00:02:38 +0200

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   const params = new URLSearchParams({
     client_id: process.env.SPOTIFY_CLIENT_ID,
     response_type: "code",
-    redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+    redirect_uri: process.env.SPOTIFY_REDIRECT_URI + "2",
     scope: "user-read-playback-state user-modify-playback-state user-read-currently-playing",
   });
   res.redirect(`https://accounts.spotify.com/authorize?${params.toString()}`);
